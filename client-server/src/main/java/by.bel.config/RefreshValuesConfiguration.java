cad2ece0483bbacd161d857b9dc187908d3906e1
brrshh <brrshh@gmail.com>
2016-07-20 17:41:03 +0300

    4

---
+++ client-server/src/main/java/by.bel.config/RefreshValuesConfiguration.java
@@ -0,0 +1,19 @@
+//package by.bel.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.cloud.context.config.annotation.RefreshScope;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * Created by Barys_Shliaha on 20-Jul-16.
+// */
+//@Configuration
+//public class RefreshValuesConfiguration {
+//
+//
+//
+//
+//    @Value("${client.messages}")
+//    private String mes;
+//}
